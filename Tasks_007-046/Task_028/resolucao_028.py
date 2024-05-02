53c730ee5c8218e03abcb044e1b98083c1f8aad3
jhonestiago <jonestiago.94@gmail.com>
2024-05-02 19:11:44 -0300

    Task 28

--- Tasks_007-046/Task_028/resolucao_028.py
+++ Tasks_007-046/Task_028/resolucao_028.py
@@ -1,3 +1,6 @@
-valor_salario:float = float(input('Insira o valor do salário: ').replace(',','.'))
-salario_minimo:float = float(input('Insira o valor do salário mínimo: ').replace(',','.'))
+salario_funcionario:float = float(input('Insira o valor do salário do funcionário: '))
+salario_minimo:float = float(input('Insira o valor do salário mínimo: '))
 
+total_salarios = salario_funcionario / salario_minimo
+
+print(f'O funcionario ganha {total_salarios:.2f} salários mínimos')
