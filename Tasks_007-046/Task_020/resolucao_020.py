12245ea55cb6ac8b7604ef0b42158857d572f07a
jhonestiago <jonestiago.94@gmail.com>
2024-05-02 15:45:04 -0300

    Task 020

---
+++ Tasks_007-046/Task_020/resolucao_020.py
@@ -0,0 +1,20 @@
+x:float = 0
+
+# Algoritmo Valor_X
+print('Valor de x')
+x = ((25 + 100 / 2) ** 2) - 1
+x = x * x / 2
+print(x)
+
+# Algoritmo Valor_X2
+x = (8 ** 2) % 3
+x += 2
+print(x)
+
+# Algoritmo Valor_X3
+x = 8
+x -= 1
+x *= 2
+x /= 2
+x += 1
+print(x)
