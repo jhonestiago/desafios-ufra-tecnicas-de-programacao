51b0b1c7666b76dbedbabaab8b7ec8f161b9d59d
jhonestiago <jonestiago.94@gmail.com>
2024-05-01 23:47:47 -0300

    Task 9

---
+++ 007-045_Algoritmo_Simples/Task_009/resolucao_009.py
@@ -0,0 +1,7 @@
+nome:str = 'João Silva'
+endereco:str = 'Rua 7 de Setembro, Nº 1822'
+telefone:str = '(XX) XXXX-XXXX'
+
+print(f'Nome: {nome}')
+print(f'Endereço: {endereco}')
+print(f'Telefone: {telefone}')
