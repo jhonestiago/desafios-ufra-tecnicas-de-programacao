ad3080643963e4de03390c469480c4550b807d1c
jhonestiago <jonestiago.94@gmail.com>
2024-05-02 13:53:52 -0300

    Task 15

--- Tasks_007-046/Task_015/resolucao_015.py
+++ Tasks_007-046/Task_015/resolucao_015.py
@@ -2,6 +2,6 @@ armazenamento_total_GB:int = 8
 espaco_ocupado_MB:int = 3584
 
 espaco_ocupado_GB = espaco_ocupado_MB / 1000
-espaco_disponível_GB = armazenamento_total_GB - espaco_ocupado_GB
+espaco_disponivel_GB = armazenamento_total_GB - espaco_ocupado_GB
 
-print(f'O pendrive ainda pode armazenar {espaco_disponível_GB} GB.')
+print(f'O pendrive ainda pode armazenar {espaco_disponivel_GB} GB.')
