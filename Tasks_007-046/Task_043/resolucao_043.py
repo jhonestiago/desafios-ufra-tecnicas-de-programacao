4f65dc5b96e77e300f17ff5d1a6c007b35ad63ab
jhonestiago <jonestiago.94@gmail.com>
2024-05-03 15:41:49 -0300

    Task 43

--- Tasks_007-046/Task_043/resolucao_043.py
+++ Tasks_007-046/Task_043/resolucao_043.py
@@ -4,4 +4,4 @@ raio:float = float(input('Insira a medida do raio do círculo (em cm): '))
 
 area_circulo = math.pi * (raio ** 2)
 
-print(f'A área do círculo é: {area_circulo} cm^2')
+print(f'A área do círculo é: {area_circulo:.2f} cm^2')
