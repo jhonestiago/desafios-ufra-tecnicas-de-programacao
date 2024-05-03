6790c5a9da26e37bb9e3c2e6ce9429fddf841aa2
jhonestiago <jonestiago.94@gmail.com>
2024-05-03 15:02:15 -0300

    Task 40

---
+++ Tasks_007-046/Task_040/resolucao_040.py
@@ -0,0 +1,68 @@
+fahrenheit = 50 # F
+
+celsius = (5 / 9) * (fahrenheit - 32)
+
+print('Tab. 1 - Relação Fahrenheit - Celsius')
+print('-------------------------------------')
+print('Fahrenheit\tCelsius')
+print(f'{fahrenheit} F\t\t{celsius:.2f} °C')
+
+fahrenheit += 1 # 51 F
+celsius = (5 / 9) * (fahrenheit - 32)
+print(f'{fahrenheit} F\t\t{celsius:.2f} °C')
+
+fahrenheit += 1 # 52 F
+celsius = (5 / 9) * (fahrenheit - 32)
+print(f'{fahrenheit} F\t\t{celsius:.2f} °C')
+
+fahrenheit += 1 # 53 F
+celsius = (5 / 9) * (fahrenheit - 32)
+print(f'{fahrenheit} F\t\t{celsius:.2f} °C')
+
+fahrenheit += 1 # 54 F
+celsius = (5 / 9) * (fahrenheit - 32)
+print(f'{fahrenheit} F\t\t{celsius:.2f} °C')
+
+fahrenheit += 1 # 55 F
+celsius = (5 / 9) * (fahrenheit - 32)
+print(f'{fahrenheit} F\t\t{celsius:.2f} °C')
+
+fahrenheit += 1 # 56 F
+celsius = (5 / 9) * (fahrenheit - 32)
+print(f'{fahrenheit} F\t\t{celsius:.2f} °C')
+
+fahrenheit += 1 # 57 F
+celsius = (5 / 9) * (fahrenheit - 32)
+print(f'{fahrenheit} F\t\t{celsius:.2f} °C')
+
+fahrenheit += 1 # 58 F
+celsius = (5 / 9) * (fahrenheit - 32)
+print(f'{fahrenheit} F\t\t{celsius:.2f} °C')
+
+fahrenheit += 1 # 59 F
+celsius = (5 / 9) * (fahrenheit - 32)
+print(f'{fahrenheit} F\t\t{celsius:.2f} °C')
+
+fahrenheit += 1 # 60 F
+celsius = (5 / 9) * (fahrenheit - 32)
+print(f'{fahrenheit} F\t\t{celsius:.2f} °C')
+
+fahrenheit += 1 # 61 F
+celsius = (5 / 9) * (fahrenheit - 32)
+print(f'{fahrenheit} F\t\t{celsius:.2f} °C')
+
+fahrenheit += 1 # 62 F
+celsius = (5 / 9) * (fahrenheit - 32)
+print(f'{fahrenheit} F\t\t{celsius:.2f} °C')
+
+fahrenheit += 1 # 63 F
+celsius = (5 / 9) * (fahrenheit - 32)
+print(f'{fahrenheit} F\t\t{celsius:.2f} °C')
+
+fahrenheit += 1 #64 F
+celsius = (5 / 9) * (fahrenheit - 32)
+print(f'{fahrenheit} F\t\t{celsius:.2f} °C')
+
+fahrenheit += 1 # 65 F
+celsius = (5 / 9) * (fahrenheit - 32)
+print(f'{fahrenheit} F\t\t{celsius:.2f} °C')
