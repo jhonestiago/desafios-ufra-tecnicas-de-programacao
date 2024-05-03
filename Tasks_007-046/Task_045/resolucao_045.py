469df5197046f3e900f7205dfeac4213044c120f
jhonestiago <jonestiago.94@gmail.com>
2024-05-03 16:06:34 -0300

    Task 45

---
+++ Tasks_007-046/Task_045/resolucao_045.py
@@ -0,0 +1,6 @@
+base:float = float(input('Insira a medida da base do retângulo (em cm): '))
+altura:float = float(input('Insira a medida da altura do retângulo (em cm): '))
+
+area_retangulo = base * altura
+
+print(f'A área do retângulo é: {area_retangulo:.2f} cm^2')
---
+++ Tasks_007-046/Task_045/task_045.md
@@ -0,0 +1,13 @@
+# Task 45
+
+## PT
+
+Faça um programa que calcule e imprima a área de um retângulo.
+
+$$Area = base \times altura$$
+
+## EN
+
+Create a program that calculates and prints the area of a rectangle.
+
+$$Area = length \times width$$
