b436c90d0f18ddb38fddff3e6bf7faa6cb67e192
jhonestiago <jonestiago.94@gmail.com>
2024-05-03 13:25:01 -0300

    Task 37

---
+++ Tasks_007-046/Task_037/resolucao_037.py
@@ -0,0 +1,8 @@
+a:int = int(input('Insira um número inteiro qualquer: '))
+b:int = int(input('Insira um número inteiro qualquer: '))
+
+a_elev_b = (a ** b)
+b_elev_a = (b ** a)
+
+print(f'{a}^{b} = {a_elev_b}')
+print(f'{b}^{a} = {b_elev_a}')
---
+++ Tasks_007-046/Task_037/task_037.md
@@ -0,0 +1,9 @@
+# Task 37
+
+## PT
+
+Faça um algoritmo/programa que receba dois números, calcule e imprima um elevado ao outro.
+
+## EN
+
+Create an algorithm/program that receives two numbers, calculates and prints one raised to the other.
